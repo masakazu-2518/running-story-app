d14ed0462195eda41fb87c4fbb9fe33ab9aeb626
masakazu-kubota <pray89mk@gmail.com>
2026-05-13 20:29:05 +0900

    Fix mobile viewport height

--- app.py
+++ app.py
@@ -933,5 +933,4 @@ import os
 
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="127.0.0.1", port=5000)
